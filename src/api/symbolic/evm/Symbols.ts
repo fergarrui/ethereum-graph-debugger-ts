c9707cdb8b9b5c303645a272932f2fdc8e19cb81
fergarrui <fergarrui@gmail.com>
2018-12-02 09:09:38 +0000

    SLOAD Bugfix

--- src/api/symbolic/evm/Symbols.ts
+++ src/api/symbolic/evm/Symbols.ts
@@ -1,6 +1,8 @@
 export enum Symbols {
   // temporal placeholder until full symexec is implemented
   UNKNOWN = 'UNKNOWN',
+  EMPTY = 'EMPTY',
+  
   ADDRESS = 'ADDRESS',
   ORIGIN = 'ORIGIN',
   CALLER = 'CALLER',
--- src/api/symbolic/evm/exec/Sload.ts
+++ src/api/symbolic/evm/exec/Sload.ts
@@ -1,11 +1,17 @@
 import { Executor } from './Executor'
 import { EVM } from '../EVM'
 import { Operation } from '../../../bytecode/Operation'
+import { Word } from '../Word';
+import { Symbols } from '../Symbols';
 
 export class Sload implements Executor {
   execute(op: Operation, evm: EVM) {
     const slot = evm.stack.pop()
-    const value = evm.storage.load(slot)
+    let value = evm.storage.load(slot)
+    if (!value) {
+      // This only has sense during CFG creation
+      value = Word.createSymbolic(Symbols.UNKNOWN)
+    }
     evm.stack.push(value)
   }
 }
