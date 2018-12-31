d80be21c0d8e3e183024b4bcc3758c4994ecfc77
fergarrui <fergarrui@gmail.com>
2018-12-31 07:38:07 +0000

    Implement select lines method in editor

--- src/client/components/Editor/AceEditor/main.js
+++ src/client/components/Editor/AceEditor/main.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import brace from 'brace';
+// var Range = require('ace/range').Range
 
 if (typeof window !== 'undefined') {
   const ace = require('brace');
@@ -21,7 +22,6 @@ class CodeEditor extends Component {
 
       require(`brace/mode/${mode}`);
       require(`brace/theme/${theme}`);
-
       const editor = ace.edit(`ace-editor-${index}`);
       this.editor = editor;
       editor.getSession().setMode(`ace/mode/${mode}`);
@@ -30,9 +30,57 @@ class CodeEditor extends Component {
       editor.on('change', e => onChange(editor.getValue(), e));
       editor.setReadOnly(setReadOnly);
       editor.setValue(setValue);
+      this.selectLines(0,0)
     }
   }
 
+  selectLines(begin, end) {
+    const {
+      index
+    } = this.props;
+    const editor = ace.edit(`ace-editor-${index}`);
+    const lines = editor.session.doc.getAllLines();
+    console.log(lines)
+    let s = begin;
+    let e = end;
+    let startRow = 0;
+    let startCol = 0;
+    let endRow = 0;
+    let endCol = 0;
+    let startFound = false;
+    let endFound = false;
+    if (begin >= end) {
+        const temp = begin;
+        begin = end;
+        end = temp;
+    }
+    for(let i = 0; i < lines.length; i++) {
+        const line = lines[i];
+        if (s > line.length && !startFound) {
+            startRow++;
+            // +1 because of new line
+            s -= line.length + 1;
+        } else {
+            startCol = s;
+            startFound = true;
+        }
+        if (e > line.length && !endFound) {
+            endRow++;
+            // + 1 because of new line
+            e -= line.length + 1;
+        } else {
+            endCol = e;
+            endFound = true;
+        }
+        if (startFound && endFound) {
+            break;
+        }
+    }
+    const Range = ace.acequire('ace/range').Range;
+    const range = new Range(startRow, startCol, endRow, endCol);
+    editor.gotoLine(startRow, 0, true);
+    editor.session.selection.setSelectionRange(range, false);
+  }
 
   shouldComponentUpdate() {
     return false;
