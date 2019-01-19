781b5e92e7e9cfc136ec7cb3300fc4fd784dd972
Stefania <s.occhilupo87@gmail.com>
2019-01-19 18:07:27 +0000

    create modal for debug transaction api call

--- src/client/components/Store/Actions.js
+++ src/client/components/Store/Actions.js
@@ -1,8 +1,22 @@
-import { SELECT_EDITOR_LINES } from './Constants.js';
+import { SELECT_EDITOR_LINES, OPEN_MODAL, CLOSE_MODAL } from './Constants.js';
 
-export default function selectEditorLines(lines) {
+export function selectEditorLines(lines) {
   return {
     type: SELECT_EDITOR_LINES,
     lines,
+  }
+}
+
+export function openModal() {
+  return {
+    type: OPEN_MODAL,
+    open: true,
+  }
+}
+
+export function closeModal() {
+  return {
+    type: CLOSE_MODAL,
+    open: false,
   }
 }
--- src/client/components/Store/Constants.js
+++ src/client/components/Store/Constants.js
@@ -1,1 +1,3 @@
 export const SELECT_EDITOR_LINES = 'SELECT_EDITOR_LINES';
+export const OPEN_MODAL = 'OPEN_MODAL';
+export const CLOSE_MODAL = 'CLOSE_MODAL';
