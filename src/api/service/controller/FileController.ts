7d09ac46979fa5c50b7b72eeebc83235e31ccad1
fergarrui <fergarrui@gmail.com>
2019-01-23 20:40:59 +0000

    Fix error in FileController

--- src/api/service/controller/FileController.ts
+++ src/api/service/controller/FileController.ts
@@ -3,7 +3,6 @@ import { provideSingleton, inject } from '../../../inversify/ioc'
 import { ContractFile } from '../bean/ContractFile'
 import { TYPES } from '../../../inversify/types'
 import { FileService } from '../service/FileService'
-import Errors from './Errors'
 
 @Route('files')
 @provideSingleton(FileController)
@@ -14,10 +13,14 @@ export class FileController extends Controller {
 
   @Get('{dir}')
   async findContractsInDir(@Path() dir: string, @Query('extension') extension: string): Promise<ContractFile[]> {
-    const contracts = await this.fileService.findContractssWithExtension(dir, extension)
-    if (contracts.length === 0) {
-      throw Errors.createError(404, `No contracts found at ${dir} with extension ${extension}`)
+    try {
+      const contracts = await this.fileService.findContractssWithExtension(dir, extension)
+      if (contracts.length === 0) {
+        throw new Error(`No contracts found at ${dir} with extension ${extension}`)
+      }
+      return contracts
+    } catch (err) {
+      throw new Error(err.message)
     }
-    return contracts
   }
 }
