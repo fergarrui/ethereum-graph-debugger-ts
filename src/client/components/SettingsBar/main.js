4dcfb7d19be655a6897c894ded005fb9909f0bfe
Stefania <s.occhilupo87@gmail.com>
2019-02-02 19:54:12 +0000

    abilitate clear storage on click

--- src/client/components/SettingsBar/main.js
+++ src/client/components/SettingsBar/main.js
@@ -7,65 +7,109 @@ import styles from '../../styles/SettingsBar.scss';
 const cx = classnames.bind(styles);
 
 
-const SettingsBar = ({ active, onButtonClick }) => {
+class SettingsBar extends React.Component {
+  constructor(props) {
+    super(props);
 
-  const handleInputChange = (event) => {    
+    this.state = {
+      configCleared: false,
+    }
+
+    this.handleInputChange = this.handleInputChange.bind(this);
+    this.handleKeyUp = this.handleKeyUp.bind(this);
+  }
+
+  handleInputChange(event) {    
     const { name, value } = event.target; 
+
+    this.setState({
+      [name]: value,
+      configCleared: false,
+    });
 
     localStorage.setItem(name, value);
   }
 
-  const handleKeyUp = (event) => {
+  handleKeyUp(event) {    
+    const { onSaveButtonClick } = this.props;
+
+
     if(event.keyCode !== 13) {
       return;
     }
-    onButtonClick();
+
+    onSaveButtonClick();
   }
 
-  const settingsBarClasses = cx({
-    'settings-bar': true,
-    'settings-bar--active': !!active
-  });
+  handleClick() {
+    localStorage.clear();
 
-    return (
-      <div className={settingsBarClasses}>
-        <div className={styles['settings-bar__item']}>
-          <input
-            name='host' 
-            placeholder={localStorage.getItem('host') || 'Blockchain host (default: 127.0.0.1:8545)'}  
-            onChange={handleInputChange}
-            onKeyUp={handleKeyUp}
-           />
+    this.setState({
+      configCleared: true,
+    });
+  }
+
+  handleSaveClick() {
+    const { onSaveButtonClick } = this.props;
+
+    onSaveButtonClick();
+  }
+
+  render() {
+    const { active } = this.props;
+    const { configCleared } = this.state;
+
+    const settingsBarClasses = cx({
+      'settings-bar': true,
+      'settings-bar--active': !!active
+    });
+  
+      return (
+        <div className={settingsBarClasses}>
+          <div className={styles['settings-bar__item']}>
+            <input
+              name='host' 
+              placeholder={localStorage.getItem('host') || 'Blockchain host (default: 127.0.0.1:8545)'}  
+              onChange={this.handleInputChange}
+              onKeyUp={this.handleKeyUp}
+             />
+          </div>
+          <div className={styles['settings-bar__item']}>
+            <input
+              name='protocol' 
+              placeholder={localStorage.getItem('protocol') || 'Blockchain protocol (default: http)'} 
+              onChange={this.handleInputChange}
+              onKeyUp={this.handleKeyUp}
+             />
+          </div>
+          <div className={styles['settings-bar__item']}>
+            <input
+              name='username'
+              placeholder={localStorage.getItem('username') || 'Blockchain basic auth username'} 
+              onChange={this.handleInputChange}
+              onKeyUp={this.handleKeyUp}
+             />
+          </div>
+          <div className={styles['settings-bar__item']}>
+            <input
+              name='password' 
+              placeholder={localStorage.getItem('password') || 'Blockchain basic auth password'} 
+              onChange={this.handleInputChange}
+              onKeyUp={this.handleKeyUp}
+             />
+          </div>
+          <div className={styles['settings-bar__item']}>
+            <button onClick={() => this.handleSaveClick()}><span>Save and Close</span></button>
+            <button onClick={() => this.handleClick()}><span>Clear Config</span></button>
+          </div>
+          <div className={styles['settings-bar__item']}>
+            {
+              configCleared && <p>Config cleared</p>
+            }
+          </div>
         </div>
-        <div className={styles['settings-bar__item']}>
-          <input
-            name='protocol' 
-            placeholder={localStorage.getItem('protocol') ||'Blockchain protocol (default: http)'} 
-            onChange={handleInputChange}
-            onKeyUp={handleKeyUp}
-           />
-        </div>
-        <div className={styles['settings-bar__item']}>
-          <input
-            name='username'
-            placeholder={localStorage.getItem('username') || 'Blockchain basic auth username'} 
-            onChange={handleInputChange}
-            onKeyUp={handleKeyUp}
-           />
-        </div>
-        <div className={styles['settings-bar__item']}>
-          <input
-            name='password' 
-            placeholder={localStorage.getItem('password') || 'Blockchain basic auth password'} 
-            onChange={handleInputChange}
-            onKeyUp={handleKeyUp}
-           />
-        </div>
-        <div className={styles['settings-bar__item']}>
-          <button onClick={onButtonClick}><span>Save and Close</span></button>
-        </div>
-      </div>
-    )
+      )
+  }
 }
 
 SettingsBar.displayName = 'SettingsBar';
